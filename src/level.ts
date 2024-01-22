26cfaffcae74607423141b6f7fa21389b1768bf1
Switt Kongdachalert <switt1995@gmail.com>
2024-01-23 06:09:03 +0700

    welp something is broken

--- src/level.ts
+++ src/level.ts
@@ -1,5 +1,5 @@
 import { AbstractBatchOperation, AbstractBatchOptions, AbstractClearOptions, AbstractDatabaseOptions, AbstractDelOptions, AbstractGetManyOptions, AbstractGetOptions, AbstractIterator, AbstractIteratorOptions, AbstractKeyIterator, AbstractKeyIteratorOptions, AbstractLevel, AbstractOpenOptions, AbstractPutOptions, AbstractSeekOptions, NodeCallback } from 'abstract-level';
-import { LevelDB, LevelDBIterator, } from '@switt/react-native-leveldb';
+import { LevelDB, LevelDBIterator, } from 'react-native-leveldb';
 import ModuleError from 'module-error';
 import type { NextCallback } from 'abstract-level/types/abstract-iterator';
 import { arrayBufferEqual } from './arrayBufferEqual';
